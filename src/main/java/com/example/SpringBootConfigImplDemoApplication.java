c4e7a367e88882508db995f00f92d8875751a237
Nilendra <nilendrayadav785@gmail.com>
2025-11-07 10:32:25 +0530

    Initial commit - SpringBootConfigImplDemo project

---
+++ src/main/java/com/example/SpringBootConfigImplDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConfigImplDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootConfigImplDemoApplication.class, args);
+	}
+
+}
